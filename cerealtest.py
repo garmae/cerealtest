0511a2db3fe3fc09205fb5884d49a33fea739984
Guillermo Garcia M <garciamaynezg@gmail.com>
2022-02-03 17:52:58 -0600

    Minor refactorings

--- cerealtest.py
+++ cerealtest.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2021 Guillermo Eduardo Garcia Maynez Rocha.
+# Copyright (C) 2021-2022 Guillermo Eduardo Garcia Maynez Rocha.
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
@@ -42,7 +42,7 @@ def is_hex_string(string: str):
     return False
 
 
-def setup_serial(config):
+def serial_init(config):
     ser.baudrate = int(config['baudRate'])
     ser.port = config['port']
     ser.timeout = config['timeout']
@@ -66,6 +66,7 @@ class Test(object):
     """Individual Test definition"""
 
     def __init__(self, test_dict):
+        # JSON defined attributes
         self.name = None
         self.is_hex = None
         self.message = None
@@ -80,11 +81,17 @@ class Test(object):
             if not is_hex_string(self.message):
                 raise Exception("Message is not HEX and it is marked as such")
 
+    def validate_regex(self, response):
+        return True if re.fullmatch(self.expected_regex, response) else False
+
+    def validate_response(self):
+        pass
+
     def __print_details(self):
         radix = ' (HEX)' if self.is_hex else ''
-        print(f'Message sent{radix}:\n{textwrap.fill(self.message, width=64)}')
+        print(f'Message to be sent:\n{textwrap.fill(self.message, width=64)}{radix}')
         print(f'Expected Regex: {self.expected_regex}')
-        print(f'Delay: {self.delay}')
+        print(f'Delay: {self.delay}s')
         print(f'Script: {self.script}')
 
     def run(self):
@@ -95,12 +102,12 @@ class Test(object):
         data = bytearray.fromhex(self.message) if self.is_hex else str.encode(self.message)
         ser.write(data)
         time.sleep(self.delay)
-        response = ser.readline()
+        raw_response = ser.readline()
 
         if self.is_hex:
-            response = textwrap.fill(response.hex().upper(), width=64) + ' (HEX)'
+            response = textwrap.fill(raw_response.hex().upper(), width=64) + ' (HEX)' if self.is_hex else raw_response.decode('ascii')
         else:
-            response = response.decode('ascii')
+            response = raw_response.decode('ascii')
         print(f'\nResponse:\n{response}')
 
         if self.script is not None:
@@ -138,7 +145,7 @@ def load_config_file(path):
         working_directory = config_data['workingDirectory']
         testing_type = config_data['testingType']
 
-        setup_serial(config_data['serialConfig'])
+        serial_init(config_data['serialConfig'])
 
         for test_spec in config_data['tests']:
             test_collection.append(Test(test_spec))
@@ -160,7 +167,7 @@ def parse_args():
 
 if __name__ == '__main__':
     print('CerealTest v0.2')
-    print('Copyright (c) 2021 Guillermo Eduardo Garcia Maynez Rocha.\n')
+    print('Copyright (c) 2021-2022 Guillermo Eduardo Garcia Maynez Rocha.\n')
     parse_args()
 
     if testing_type == 'continuous':
